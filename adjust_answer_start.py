d9a8ffbcabcd4a65550ad7029a9619b074ed49b6
Sebastian Reimer <ser@bd-a.de>
2020-06-21 08:14:08 +0200

    adjusted start of plausible answers

--- adjust_answer_start.py
+++ adjust_answer_start.py
@@ -53,62 +53,15 @@ for ct, item in enumerate(squad_small['data']): #TODO change dict
             for qas in item[key]['qas']:
                 print("-----qas-------")
                 print(qas.keys())
-                list2keep = []
-                for ctr, answer in enumerate(qas['answers']):
-                    #print("--------------------")
-                    #print(f">>>answer before:")
-                    #print(answer)
-                    #define start and end string
-                    if answer['answer_start'] < 100:
-                        tmp_start = 0
-                    else:
-                        tmp_start = answer['answer_start'] - 100
-                    if answer['answer_start'] + 100 > len_con:
-                        tmp_end = len_con
-                    else:
-                        tmp_end = answer['answer_start'] + 100
-                    
-                    substring = context[tmp_start:tmp_end] #extract potentially relevant substring
-                    
-                    match = re.search(answer['text'], substring) #check if substring contains answer
-                    #print(match)
-                    if match: #context contains anwer
-                        tmp_start_substring = substring.find(answer['text'])
-                        #print(tmp_start_substring)
-
-                        if tmp_start_substring != -1: # in case answer is not found
-                            answer_start = tmp_start + tmp_start_substring    # adapt start of answer from substring to context
-                            answer['answer_start'] = answer_start
-                            list2keep.append(answer)
-                            #print(answer['text'])
-                            #print(answer['answer_start'])
-                            #print(context[answer_start:answer_start+35])
-                       # else: #context does not contain answer
-                       #     print(f">>>answer deleted 1:")
-                       #     print(answer)
-                       #     #del qas['answers'][ctr]
-                       #     list2delete.append(ctr)
-
-                        #TODO for plausible answers
-                        #print(f">>>answer after:")
+                
+                # adust answer_start in answers
+                if "answers" in qas.keys():
+                    print("xxxxxxxxxxxxxxxxxxxxx")
+                    list2keep = []
+                    for ctr, answer in enumerate(qas['answers']):
+                        #print("--------------------")
+                        #print(f">>>answer before:")
                         #print(answer)
-
-                    #else: #context does not contain answer
-                        #print(f">>>answer deleted 2:")
-                        #print(answer)
-                        #list2delete.append(ctr)
-
-                #only keep answer which answers appear in context
-                qas['answers'] = list2keep
-
-                list2keep = []
-########################################hier weiter machen##########################################
-                if qas['plausible_answers']:
-
-                    for ctr, answer in enumerate(qas['plausible_answers']):
-                        print("--------------------")
-                        print(f">>>answer before:")
-                        print(answer)
                         #define start and end string
                         if answer['answer_start'] < 100:
                             tmp_start = 0
@@ -131,13 +84,65 @@ for ct, item in enumerate(squad_small['data']): #TODO change dict
                                 answer_start = tmp_start + tmp_start_substring    # adapt start of answer from substring to context
                                 answer['answer_start'] = answer_start
                                 list2keep.append(answer)
-                        print(f">>>answer after:")
-                        print(answer)
+                                #print(answer['text'])
+                                #print(answer['answer_start'])
+                                #print(context[answer_start:answer_start+35])
+                        # else: #context does not contain answer
+                        #     print(f">>>answer deleted 1:")
+                        #     print(answer)
+                        #     #del qas['answers'][ctr]
+                        #     list2delete.append(ctr)
 
+                            #TODO for plausible answers
+                            #print(f">>>answer after:")
+                            #print(answer)
+
+                        #else: #context does not contain answer
+                            #print(f">>>answer deleted 2:")
+                            #print(answer)
+                            #list2delete.append(ctr)
 
                     #only keep answer which answers appear in context
-                    qas['plausible_answers'] = list2keep
-########################################hier weiter machen##########################################
+                    qas['answers'] = list2keep
+                    
+                    
+                # do it also for plausible answers
+                if "plausible_answers" in qas.keys():
+                        print("uuuuuuuuuuuuuuuuuuuuu")
+                        list2keep = []
+                        for ctr, answer in enumerate(qas['plausible_answers']):
+                            print("--------------------")
+                            print(f">>>answer before:")
+                            print(answer)
+                            #define start and end string
+                            if answer['answer_start'] < 100:
+                                tmp_start = 0
+                            else:
+                                tmp_start = answer['answer_start'] - 100
+                            if answer['answer_start'] + 100 > len_con:
+                                tmp_end = len_con
+                            else:
+                                tmp_end = answer['answer_start'] + 100
+                            
+                            substring = context[tmp_start:tmp_end] #extract potentially relevant substring
+                            
+                            match = re.search(answer['text'], substring) #check if substring contains answer
+                            #print(match)
+                            if match: #context contains anwer
+                                tmp_start_substring = substring.find(answer['text'])
+                                #print(tmp_start_substring)
 
-print(squad_small)                    
+                                if tmp_start_substring != -1: # in case answer is not found
+                                    answer_start = tmp_start + tmp_start_substring    # adapt start of answer from substring to context
+                                    answer['answer_start'] = answer_start
+                                    list2keep.append(answer)
+                            print(f">>>answer after:")
+                            print(answer)
+
+
+                        #only keep answer which answers appear in context
+                        qas['plausible_answers'] = list2keep
+########################################hier weiter machen##########################################
+                        
+#print(squad_small)                    
 
